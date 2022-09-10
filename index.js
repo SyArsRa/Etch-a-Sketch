e939f5434891b861e8c8086a056b1ce34dce9851
Syed Arsal Rahman <arsalazfar@hotmail.com>
2022-09-10 20:01:28 +0400

    Added colors and fixed size change, project completed

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const grid = document.getElementById('grid');
 const clear = document.getElementById('clear');
 const val = document.getElementById('value');
 const slider = document.getElementById('size');
-
+const colorPick = document.querySelector('.colorPick')
+let color = "black"
 createGrid(16)
 
-slider.onmousemove = slider.onchange = function() {
+colorPick.oninput = function(){
+  colorPick.style.backgroundColor = colorPick.value
+  colorPick.style.borderColor = colorPick.value
+  color = colorPick.value
+}
+
+slider.onmousemove = function() {
+  val.innerHTML = `Size:${slider.value}`
+}
+slider.onchange  = function() {
   val.innerHTML = `Size:${slider.value}`
   del(slider.value)
 }
@@ -20,6 +30,8 @@ document.onmouseup = function(){
 
 clear.addEventListener("click", del);
 function createGrid(size){
+  dim = 320/size
+  console.log(dim)
   for(var x = 0 ; x < size ; x++){
     let row = document.createElement('span');
     row.setAttribute("class","row");
@@ -29,6 +41,7 @@ function createGrid(size){
       cell.setAttribute("id",(`${String(x)}x${String(y)}`))
       cell.addEventListener('mouseover', colorChange);
       cell.addEventListener('click', colorChange);
+      cell.style=`width:${dim}px; height:${dim}px`;
       row.append(cell);
     }
     grid.append(row)
@@ -36,9 +49,9 @@ function createGrid(size){
 }
 function colorChange(event){
     if (event.type == "mouseover" && !mouseDown ) return
-    event.target.style.backgroundColor = "black";
+    event.target.style.backgroundColor = color;
 }
 function del(size){
   grid.innerHTML = '';
-  createGrid(size)
+  createGrid(slider.value)
 }
